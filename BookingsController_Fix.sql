da951a9387b5d2b49a18edc859008cbbdeea44c4
JTyii <ychauyuan@gmail.com>
2025-11-24 22:57:23 +0800

    .

--- BookingsController_Fix.sql
+++
@@ -1,7 +0,0 @@
--- SQL Query to check current Bookings table structure
-SELECT TOP 1 * FROM Bookings;
-
--- You need to modify your BookingsController to JOIN with Rooms table
--- to include the room's ImageUrl in the booking response.
-
--- Here's the updated C# code for your BookingsController:

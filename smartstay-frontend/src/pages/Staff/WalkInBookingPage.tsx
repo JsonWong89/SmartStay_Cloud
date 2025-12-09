7c54bbb12b324e6eb91ee4f91175be83677471a2
nieshaa <monishaa@myinvoice2u.com>
2025-12-09 09:31:45 +0800

    design changes

--- smartstay-frontend/src/pages/Staff/WalkInBookingPage.tsx
+++ smartstay-frontend/src/pages/Staff/WalkInBookingPage.tsx
@@ -538,7 +538,7 @@ function WalkInBookingContent({
                     )}
                   </div>
 
-                  <div className="md:col-span-2">
+                  <div>
                     <label className="block text-sm font-medium text-gray-700 mb-2">
                       Email Address <span className="text-red-500">*</span>
                     </label>
